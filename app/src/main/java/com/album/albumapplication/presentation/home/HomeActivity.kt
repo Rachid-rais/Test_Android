d8a24658c8e743ae9ef919a7defdf2dad3bf804c
Rachid <rachid.rais@laposte.fr>
2022-08-29 20:53:09 +0200

    +[noIssue] Add animation loader Splash screen + viewbinding

--- app/src/main/java/com/album/albumapplication/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/album/albumapplication/presentation/home/HomeActivity.kt
@@ -2,14 +2,22 @@ package com.album.albumapplication.presentation.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.album.albumapplication.R
+import com.album.albumapplication.databinding.ActivityHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(root)
+        }
     }
 }
--- app/src/main/java/com/album/albumapplication/presentation/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/album/albumapplication/presentation/splashscreen/SplashActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.album.albumapplication.R
+import com.album.albumapplication.databinding.ActivitySplashscreenBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashscreenBinding
 
     private val splashViewModel: SplashViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(root)
+        }
 
     }
 
